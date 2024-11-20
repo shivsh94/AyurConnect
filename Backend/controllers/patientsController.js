644fbcd51b5d40d259367b676521c8585bd6d063
shivsh94 <shivamsharma23jan@gmail.com>
2024-11-20 12:15:37 +0530

    Remove unused files and configurations from the Backend node_modules directory

--- Backend/controllers/patientsController.js
+++ Backend/controllers/patientsController.js
@@ -1,7 +1,6 @@
-import patientsRegistrationfrom "../models/patientsRegistration";
+// import patientsRegistrationfrom "../models/patientsRegistration";
 
-export const patientsRegistration = async (req, res) => {
-  try {
-    const { "Patient's name": patientName, "phone no": phoneNo, address, age, height,
-         weight, gender, bloodGroup, disease, "Any medical history": medicalHistory, "Any previous report": previousReport } = req.body;
-         
+// export const patientsRegistration = async (req, res) => {
+//   try {
+//     const { "Patient's name": patientName, "phone no": phoneNo, address, age, height,
+//          weight, gender, bloodGroup, disease, "Any medical history": medicalHistory, "Any previous report": previousReport } = req.body;
